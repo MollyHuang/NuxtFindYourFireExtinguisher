d8a851065abcbc46702f945f785d25d613bf099d
Molly Huang <molly.h@gmail.com>
2021-08-01 17:37:02 +0800

    1:22:13 - Routing & Pages

--- components/LargeCard.vue
+++ components/LargeCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <NuxtLink :to="`/products/${card.id}`" class="card">
     <img :src="require(`@/assets/images/${card.image || 'fe1.jpg'}`)" alt="fe" class="image">
     <h3 class="header">
       {{ card.title }}
@@ -7,7 +7,7 @@
     <p class="snippet">
       {{ card.snippet }}
     </p>
-  </div>
+  </NuxtLink>
 </template>
 
 <script>
@@ -18,22 +18,27 @@
 
 <style scoped>
   .card {
-      width: 31.5%;
-      height: 25rem;
-      border: none;
-      overflow: hidden;
-      padding: 0;
-      cursor: pointer;
+    width: 31.5%;
+    height: 25rem;
+    border: none;
+    overflow: hidden;
+    padding: 0;
+    cursor: pointer;
   }
   .image {
-      height: 65%;
-      border-radius: 0.5rem;
+    height: 65%;
+    border-radius: 0.5rem;
   }
   .header {
-      font-size: 1.15rem;
-      margin-top: 0.4rem;
+    font-size: 1.15rem;
+    margin-top: 0.4rem;
+    color: black;
+  }
+  .header:hover {
+    color: black;
+    text-decoration: none;
   }
   .snippet {
-      color: grey
+    color: grey
   }
 </style>
